102e8726520a009726f23939c59772cb7d2e7690
bumbot <pkim1295@gmail.com>
2020-02-26 12:41:05 -0500

    finished ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,5 @@
 class IngredientsController < ApplicationController
+    # before_action find, only: [:show, ]
 
     def index
         @ingredients = Ingredient.all
@@ -9,10 +10,25 @@ class IngredientsController < ApplicationController
     end
 
     def new
+        @ingredient = Ingredient.new
+    end
 
+    def create
+        @ingredient = Ingredient.create(ingredient_params)
+        redirect_to ingredient_path(@ingredient)
+    end
+
+    def destroy
+        @ingredient = Ingredient.find(params[:id])
+        @ingredient.destroy
+        redirect_to ingredients_path
     end
 
     private
+    def find
+        @ingredient = Ingredient.find(ingredient_params)
+    end
+
     def ingredient_params
         params.require(:ingredient).permit(:name)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :index, :new, :create]
-  delete "/users/:id", to: "users#destroy", as: "destroy"
+  delete "/users/:id", to: "users#destroy", as: "destroy_user"
 
-  resources :ingredients, only: [:index, :show]
+  resources :ingredients, only: [:index, :show, :new, :create]
+  delete "/ingredients/:id", to: "ingredients#destroy", as: "destroy_ingredient"
 end
